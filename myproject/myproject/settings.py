1679553be01c9d56a476b10337169c832f980856
AdmiralKeso <cas.hagenaar@gmail.com>
2024-04-10 14:14:43 +0200

    Update DEBUG to False to prevent displaying sensitive information during debugging

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ddw)qz2c&!$9r94=qow8+2)@om)lwe$q1gx1%yw-#mecqvv27w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
