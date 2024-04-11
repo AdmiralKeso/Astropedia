ee4a5c7a392d7c99708b544877fcc5bb6623bfe7
AdmiralKeso <cas.hagenaar@gmail.com>
2024-04-11 12:35:01 +0200

    Remove slug field and replaced it with text field
    Change max length of text field to 400 to warn user if text is longer then 400 letters.

--- myproject/post/models.py
+++ myproject/post/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    text = models.TextField(max_length=400)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
 )
