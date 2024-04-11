7b38af7a405b7e7195ff7154f8daf855e75a5db8
AdmiralKeso <cas.hagenaar@gmail.com>
2024-04-11 12:36:28 +0200

    Update title field to have a maximum length of 40 characters

--- myproject/post/models.py
+++ myproject/post/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=40, unique=True)
     text = models.TextField(max_length=400)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
