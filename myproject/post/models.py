12bb3da22ddf33639fbe8f95f6f57365f2a8a427
AdmiralKeso <cas.hagenaar@gmail.com>
2024-04-11 12:43:18 +0200

    Add lines and organize
    Code passed through linter.

--- myproject/post/models.py
+++ myproject/post/models.py
@@ -1,17 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Post(models.Model):
     title = models.CharField(max_length=40, unique=True)
     text = models.TextField(max_length=400)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-date_create = models.DateTimeField(auto_now_add=True)
-STATUS = ((0, "Draft"), (1, "Published"))
-status = models.IntegerField(choices=STATUS, default=0)    upvotes = models.ManyToManyField(User, related_name="upvote_posts", blank=True)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    date_create = models.DateTimeField(auto_now_add=True)
+    upvotes = models.ManyToManyField(User, related_name="upvote_posts", blank=True)
     downvotes = models.ManyToManyField(User, related_name="downvote_posts", blank=True)
     bookmarks = models.ManyToManyField(User, related_name="bookmark_posts", blank=True)
+
+    STATUS = (
+        (0, "Draft"),
+        (1, "Published"),
+    )
+    status = models.IntegerField(choices=STATUS, default=0)
+
     def total_upvotes(self):
         return self.upvotes.count()
 
